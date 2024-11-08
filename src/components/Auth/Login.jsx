b25e5b6a1656e7a5cd39ac5874d27805a6751c5b
Hridayesh1605 <hridayeshmore16@gmail.com>
2024-11-08 15:24:30 +0530

    created login page

---
+++ src/components/Auth/Login.jsx
@@ -0,0 +1,41 @@
+import React,{useState} from 'react'
+
+const Login = () => {
+
+    const [email, setemail] = useState('')
+    const [password, setpassword] = useState('')
+
+    const SubmitHandler = (e) =>{
+        e.preventDefault()
+        console.log("Email is ",email)
+        console.log("Password is ",password)
+
+        setemail('')
+        setpassword('')
+
+    }
+
+    const EmailChanger = (e) =>{
+        setemail(e.target.value)
+
+    }
+    const PassChanger = (e) =>{
+        setpassword(e.target.value)
+
+    }
+
+  return (
+    <div className='flex h-screen w-screen items-center justify-center'>
+    <div className='border-2 border-emerald-600 p-20 rounded-xl'>
+        <form onSubmit={(e)=>{SubmitHandler(e)}} className='flex flex-col items-center justify-center'>
+            <input required className='outline-none bg-transparent border-2 border-emerald-600 text-xl py-3 px-5 rounded-full placeholder:text-gray-400' type='email' placeholder='enter your email' onChange={EmailChanger} value={email}/>
+            <input className='outline-none bg-transparent border-2 border-emerald-600 text-xl py-3 px-5 rounded-full placeholder:text-gray-400 mt-5' type='password' placeholder='enter your password' onChange={PassChanger} value={password}/>
+            <button className='text-white outline-none border-2 bg-emerald-600 text-xl py-3 px-5 rounded-full placeholder:text-white mt-5 border-none'>Log in</button>
+        </form>
+    </div>
+      
+    </div>
+  )
+}
+
+export default Login
