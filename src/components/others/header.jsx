846383470b3f938ff24b0c6ea971c4e4537a0c50
Hridayesh1605 <hridayeshmore16@gmail.com>
2024-11-09 12:36:59 +0530

    created logout feature

--- src/components/others/header.jsx
+++ src/components/others/header.jsx
@@ -8,10 +8,14 @@ const header = () => {
   // }else{
   //   setUsername(data.firstName)
   // }
+  const loggedInUser =()=>{
+    localStorage.setItem('loggedInUser','')
+    window.location.reload()
+  }
   return (
     <div className='flex items-end justify-between'>
     <h1 className='text-2xl font-medium'>Hello <br/> <span className='text-3xl font-semibold'>username</span> </h1>
-    <button className='bg-red-600 text-lg font-medium text-white px-5 py-2'>Log Out</button>
+    <button className='bg-red-600 text-lg font-medium text-white px-5 py-2' onClick={loggedInUser}>Log Out</button>
       
     </div>
   )
